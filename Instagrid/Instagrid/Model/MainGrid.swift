cfc643fd7f4b324f36326a22b1c505c29b77a6ca
lauriane75 <lauriane.haydari@gmail.com>
2019-04-06 10:03:57 +0200

    Add constraints

---
+++ Instagrid/Instagrid/Model/MainGrid.swift
@@ -0,0 +1,13 @@
+//
+//  main-grid.swift
+//  Instagrid
+//
+//  Created by Lauriane Haydari on 06/04/2019.
+//  Copyright © 2019 Lauriane Haydari. All rights reserved.
+//
+
+import Foundation
+
+class MainGrid {
+    
+}
